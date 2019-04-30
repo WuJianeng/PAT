199965f27dd0c83b0e1a34849a1fee65a714790b
WuJianeng <32578154+WuJianeng@users.noreply.github.com>
2019-04-30 11:52:27 +0800

    Family Property

--- A1114.cpp
+++ A1114.cpp
@@ -1,32 +1,84 @@
 //并查集，还不会 
 //2019/3/5 15:00
+//2019 4/30 forked from LiuChuo
 #include<cstdio>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
-const int maxn = 10500;
-int n;
-vector<int> v[maxn];
-int estate[maxn][2];
-int min[maxn];
-int main(){
-	scanf("%d", &n);
-	for(int i = 0; i < n; ++i){
-		int id, father, mother, k;
-		scanf("%d %d %d %d", &id, &father, &mother, &k);
-		if(father != -1) v[id].push_back(father);
-		if(mother != -1) v[id].push_back(mother);
-		for(int j = 0; j < k; ++j){
-			int child;
-			scanf("%d", &child);
-			v[id].push_back(child);
-		}
-		scanf("%d %d", &estate[id][0], &estate[id][1]);
-	}
-	for(int i = 0; i < maxn; ++i)
-		min[maxn] = i;
-	for(int i = 0; i < n; ++i){
-		
-	}
-	return 0;
+struct DATA {
+    int id, fid, mid, num, area;
+    int cid[10];
+}data[1005];
+struct node {
+    int id, people;
+    double num, area;
+    bool flag = false;
+}ans[10000];
+int father[10000];
+bool visit[10000];
+int find(int x) {
+    while(x != father[x])
+        x = father[x];
+    return x;
+}
+void Union(int a, int b) {
+    int faA = find(a);
+    int faB = find(b);
+    if(faA > faB)
+        father[faA] = faB;
+    else if(faA < faB)
+        father[faB] = faA;
+}
+int cmp1(node a, node b) {
+    if(a.area != b.area)
+        return a.area > b.area;
+    else
+        return a.id < b.id;
+}
+int main() {
+    int n, k, cnt = 0;
+    scanf("%d", &n);
+    for(int i = 0; i < 10000; i++)
+        father[i] = i;
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d %d %d", &data[i].id, &data[i].fid, &data[i].mid, &k);
+        visit[data[i].id] = true;
+        if(data[i].fid != -1) {
+            visit[data[i].fid] = true;
+            Union(data[i].fid, data[i].id);
+        }
+        if(data[i].mid != -1) {
+            visit[data[i].mid] = true;
+            Union(data[i].mid, data[i].id);
+        }
+        for(int j = 0; j < k; j++) {
+            scanf("%d", &data[i].cid[j]);
+            visit[data[i].cid[j]] = true;
+            Union(data[i].cid[j], data[i].id);
+        }
+        scanf("%d %d", &data[i].num, &data[i].area);
+    }
+    for(int i = 0; i < n; i++) {
+        int id = find(data[i].id);
+        ans[id].id = id;
+        ans[id].num += data[i].num;
+        ans[id].area += data[i].area;
+        ans[id].flag = true;
+    }
+    for(int i = 0; i < 10000; i++) {
+        if(visit[i])
+            ans[find(i)].people++;
+        if(ans[i].flag)
+            cnt++;
+    }
+    for(int i = 0; i < 10000; i++) {
+        if(ans[i].flag) {
+            ans[i].num = (double)(ans[i].num * 1.0 / ans[i].people);
+            ans[i].area = (double)(ans[i].area * 1.0 / ans[i].people);
+        }
+    }
+    sort(ans, ans + 10000, cmp1);
+    printf("%d\n", cnt);
+    for(int i = 0; i < cnt; i++)
+        printf("%04d %d %.3f %.3f\n", ans[i].id, ans[i].people, ans[i].num, ans[i].area);
+    return 0;
 }
